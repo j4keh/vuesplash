5393977902e741bffd2aeddcf0ea22dc03e0acf2
jake <t.brightsky@gmail.com>
2020-04-20 01:59:26 +0900

    ローディング、メッセージ追加

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -4,4 +4,7 @@ mix.browserSync({
   proxy: '0.0.0.0:8081', // アプリの起動アドレス
   open: false // ブラウザを自動で開かない
 }).js('resources/js/app.js', 'public/js')
+  .styles([
+    'resources/css/app.css',
+  ], 'public/css/app.css')
   .version()
