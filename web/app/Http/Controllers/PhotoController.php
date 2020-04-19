969927182e991c0d180a6a3828cf32a1fbc7d733
jake <t.brightsky@gmail.com>
2020-04-20 01:32:13 +0900

    アップロード機能作成

--- web/app/Http/Controllers/PhotoController.php
+++ web/app/Http/Controllers/PhotoController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 class PhotoController extends Controller
 {
