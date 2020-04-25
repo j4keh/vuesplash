d4e4059b0fdee16d1db81ba5a66fe79138c465c3
jake <t.brightsky@gmail.com>
2020-04-25 19:20:32 +0900

    写真一覧表示

--- web/resources/js/pages/PhotoList.vue
+++ web/resources/js/pages/PhotoList.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Photo List</h1>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        :item="photo"
+        :key="photo.id"
+        class="grid__item"
+        v-for="photo in photos"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+
+export default {
+  components: {
+    Photo
+  },
+  data() {
+    return {
+      photos: []
+    }
+  },
+  methods: {
+    async fetchPhotos() {
+      const response = await axios.get('/api/photos')
+
+      if(response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photos = response.data.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler() {
+        await this.fetchPhotos()
+      },
+      immediate: true
+    }
+  }
+}
+</script>
