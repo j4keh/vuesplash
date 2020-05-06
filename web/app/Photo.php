496a9c25e9bebfc3aa5f96077a758331c068bd79
jake <t.brightsky@gmail.com>
2020-05-06 20:08:01 +0900

    いいねAPI追加2

--- web/app/Photo.php
+++ web/app/Photo.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class Photo extends Model
 {
@@ -19,6 +20,8 @@ class Photo extends Model
     /** JSONに含める属性 */
     protected $appends = [
         'url',
+        'likes_count',
+        'liked_by_user',
     ];
 
     /** JSONに含める属性 */
@@ -27,6 +30,8 @@ class Photo extends Model
         'owner',
         'url',
         'comments',
+        'likes_count',
+        'liked_by_user',
     ];
 
     public function __construct(array $attributes = [])
@@ -102,5 +107,29 @@ class Photo extends Model
     public function likes()
     {
         return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+        if (Auth::guest()) {
+            return false;
+        }
+
+        return $this->likes->contains(function($user) {
+            return $user->id === Auth::user()->id;
+        });
     }
 }
